8ffeb6708a0ad2786d88f0fd9f6c73e17e92d0f8
esikmalazman <opikmalazman@gmail.com>
2021-04-14 18:08:53 +0800

    add tab bar view

---
+++ Food Journal App/WelcomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Food Journal App
+//
+//  Created by Ikmal Azman on 10/04/2021.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
