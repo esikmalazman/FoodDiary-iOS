fe36be5cdac1cac44e1671df4464ab04a8bf93d1
Ikmal Azman <opikmalazman@gmail.com>
2021-08-22 19:23:44 +0800

    initialise computed property for journal description, current date, refactor logic into function, remove unused code

--- Food Journal App/Controller/SaveFoodVC.swift
+++ Food Journal App/Controller/SaveFoodVC.swift
@@ -8,140 +8,105 @@
 import UIKit
 import CoreData
 
-class SaveFoodVC: UIViewController
-{
+class SaveFoodVC: UIViewController {
     
-
-    
-    var receiveImage : UIImage?
-    //get current date
-    let currentDate = Date()
-    //initialise date formatter
-    let dateFormatter = DateFormatter()
-
     @IBOutlet weak var capturedImage: UIImageView!
-    
     @IBOutlet weak var doneButton: UIButton!
     @IBOutlet weak var journalDescriptionTextField: UITextField!
+    
+    var receiveImage : UIImage?
+    
+    private var journalDescription : String {
+        get {
+            guard let description = journalDescriptionTextField.text else { fatalError("description failed to display, description is nil")}
+            return description
+        }
+        set {
+            journalDescriptionTextField.placeholder = newValue
+        }
+    }
+    
+    private var currentDate : String {
+        get {
+            let dateFormatter = DateFormatter()
+            let date = Date()
+            dateFormatter.timeStyle = .short
+            dateFormatter.dateStyle = .long
+            let formattedDate = dateFormatter.string(from: date)
+            return formattedDate
+        }
+    }
     
     //UIApplication.shared.delegate as! AppDelegate, allow access to app delegate
     // .persistentContainer, allow access to persistent container(db)
     //.viewContext, allow to manage reference to persistent container to store & retrieve from core data
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
-    override func viewDidLoad()
-    {
-        
+    override func viewDidLoad() {
         super.viewDidLoad()
+        renderView()
+    }
+    
+    @IBAction func doneJournalButtonPressed(_ sender: UIButton) {
+        validateTextField()
+        //create notification to the notification center observer, call fetchFood() when new object loads up
+        NotificationCenter.default.post(name: NSNotification.Name(rawValue: "load"),object: nil)
+    }
+    
+    func didSaveFoodJournal() {
+        //create Food Object for data model in core data
+        
+        //1.create a new food object in context
+        let newFood = Food(context: self.context)
+        //assign data to property
+        newFood.foodDesc = journalDescription
+        newFood.date = currentDate
+        //convert UIImage to Data
+        newFood.foodImg = receiveImage?.jpegData(compressionQuality: 1)
+        
+        //2. save data, through manage object context
+        PersistentManager.shared.saveContext()
+    }
+}
+
+extension SaveFoodVC : UITextFieldDelegate {
+    //method to tel
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        
+        if  journalDescriptionTextField.text != "" {
+            resignFirstResponder()
+            return true
+        }
+        else {
+            return false
+        }
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
+
+//MARK:- Private methods
+
+extension SaveFoodVC {
+    
+    private func renderView() {
         doneButton.layer.cornerRadius = 25.0
         capturedImage.image = receiveImage
         //assign textfield delegate
         journalDescriptionTextField.delegate = self
         
-        
-
     }
     
-    
-    
-    
-    @IBAction func doneJournalButtonPressed(_ sender: UIButton)
-    {
-        dateFormatter.timeStyle = .short
-        dateFormatter.dateStyle = .long
-        
-        //get date time string from date object
-        let formatDate = dateFormatter.string(from : currentDate)
-        print(journalDescriptionTextField.text!)
-        let description = journalDescriptionTextField.text
-        
- 
-//            let foodImage = UIImage.pngData(capturedImage) as NSData?
-     
-        
-    
+    private func validateTextField() {
         //prevent empty textfield
-        if  journalDescriptionTextField.text != ""
-        {
+        if  journalDescription != "" {
+            didSaveFoodJournal()
             //go to welcome VC
             self.dismiss(animated: true, completion: nil)
-            
+        } else {
+            journalDescription = "Food Description?"
         }
-        else
-        {
-            journalDescriptionTextField.placeholder = "Food Description?"
-        }
-        
-        //create data in core data
-        
-        //1.create a new food object in context
-        
-        let newFood = Food(context: self.context)
-        //assign data to property
-        newFood.foodDesc = description
-        newFood.date = formatDate
-        //convert UIImage to Data
-        newFood.foodImg = receiveImage?.jpegData(compressionQuality: 1)
-        
-        //2. save data, through manage object context
-        do
-        {
-            print("Food is saved")
-            try self.context.save()
-        }
-        catch
-        {
-            print(error)
-        }
-        
-        //re-fetch the data
-        
-        //create notification to the notification center observer
-        NotificationCenter.default.post(name: NSNotification.Name(rawValue: "load"),object: nil)
-        
-        
-    
-    }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-}
-
-extension SaveFoodVC : UITextFieldDelegate
-{
-    //method to tel
-    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool
-    {
-        if  journalDescriptionTextField.text != ""
-        {
-            resignFirstResponder()
-            return true
-        }
-        else
-        {
-            return false
-        }
-        
-    }
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool
-    {
-        textField.resignFirstResponder()
-        return true
-    }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?)
-    {
-//        if segue.identifier == K.goToFoodJournalVCSegues
-//        {
-//            let destinationVC = segue as!  FoodJournalVC
-//        }
     }
 }
---
+++ Food Journal App/PersistentManager.swift
@@ -0,0 +1,29 @@
+//
+//  PersistentManager.swift
+//  Food Journal App
+//
+//  Created by Ikmal Azman on 22/08/2021.
+//
+
+import UIKit
+
+struct PersistentManager {
+    
+    static let shared = PersistentManager()
+    //UIApplication.shared.delegate as! AppDelegate, allow access to app delegate
+    // .persistentContainer, allow access to persistent container(db)
+    //.viewContext, allow to manage reference to persistent container to store & retrieve from core data
+    private let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+
+    func saveContext() {
+        do {
+            print("Item saved")
+            try self.context.save()
+            
+        } catch {
+            print(error)
+        }
+    }
+    
+}
