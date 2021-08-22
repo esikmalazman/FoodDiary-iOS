eb0fcbc3917bd092b796b485297bc45e603c9246
Ikmal Azman <opikmalazman@gmail.com>
2021-08-22 22:31:31 +0800

    refactor code logic, setup cell content from the cell function itself, format code

--- Food Journal App/Controller/FoodJournalVC.swift
+++ Food Journal App/Controller/FoodJournalVC.swift
@@ -7,161 +7,118 @@
 
 import UIKit
 
-//UITableViewSource, to display items in table view
-class FoodJournalVC: UIViewController,UITableViewDataSource
-{
-   
+class FoodJournalVC: UIViewController {
     
-
     @IBOutlet weak var tableView: UITableView!
-    //manage & reference to persistent container
-    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
     //array to store fetch messages, data for table
-    var items : [Food]?
+    var foodItems = [Food]()
     var foodRows : Int?
-   
     
+    let persistentManager = PersistentManager.shared
     
-   
-    override func viewDidLoad()
-    {
+    override func viewDidLoad() {
         super.viewDidLoad()
-        
-        //listening for any notifcation that already send by sender from another VC
-        NotificationCenter.default.addObserver(self, selector: #selector(fetchFood), name: NSNotification.Name(rawValue: "load"), object: nil)
-        
-        //register custom design n file to be able to use in table view
-        tableView.register(UINib(nibName: "FoodCell", bundle: nil), forCellReuseIdentifier: K.reusableCellIdentifier)
-        
-        tableView.rowHeight = UITableView.automaticDimension
-        tableView.estimatedRowHeight = 600
-
-        //assign table view to adopt delegate
-        tableView.dataSource = self
-        tableView.delegate = self
-        // Do any additional setup after loading the view.
+        renderView()
         fetchFood()
     }
     
-    @objc func fetchFood()
-    {
-        items = []
-        //Fetch the data from Core Data to display in the table view
-        do
-        {
-            //assign fetch data to array
-            //fetch request from food model, grab all the data we have
-            self.items = try context.fetch(Food.fetchRequest())
-            
-            //make ui changes in main thread, to prevent perforamance impact
-            DispatchQueue.main.async
-            {
-                //reload table view after get some data
-                self.tableView.reloadData()
-            }
-           
-        }
-        catch
-        {
-            print(error)
-        }
-      
-    }
-    
-    //determine number of cells
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
-    {
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let foodInfo = self.foodItems[foodRows!]
         
-        return self.items?.count ?? 0
-        
-    }
-    //ask table view cell that it should display in each row
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
-    {
-        //Get food from array
-        let food = self.items![indexPath.row]
-        //create a cell to return to table view
-        let cell = tableView.dequeueReusableCell(withIdentifier: K.reusableCellIdentifier, for: indexPath) as! FoodCell
-        cell.dateLabel.text = food.date
-        cell.descriptionLabel.text = food.foodDesc
-        
-        
-        // allows to remove
-        // Instance will be immediately deallocated because property ‘imageView’ is ‘weak’
-        //convert Data to UIImage
-        cell.foodImage.image = UIImage(data:food.foodImg!,scale:1.0)!
-        //set cell text
-//        cell.textLabel?.text = food.foodDesc
-        return cell
-        
-    }
-
-
-}
-
-//manage user intereaction with tableview
-//MARK:- UITableViewDelegate
-
-extension FoodJournalVC:UITableViewDelegate
-{
-    //take action on selected row
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
-    {
-        
-        foodRows = indexPath.row
-        //prefromSegues
-        self.performSegue(withIdentifier: K.goToDetailFoodVCSegues, sender: self)
-        
-        
-        
-    }
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?)
-    {
-        let foodInfo = self.items![foodRows!]
-        
-        if segue.identifier == K.goToDetailFoodVCSegues
-        {
+        if segue.identifier == K.goToDetailFoodVCSegues {
             let destinationVC = segue.destination as! DetailFoodVC
             destinationVC.date = foodInfo.date
             destinationVC.desc = foodInfo.foodDesc
             destinationVC.image = UIImage(data:foodInfo.foodImg!,scale:1.0)!
         }
     }
+}
 
+//MARK:- TableViewDataSource Methods
+
+extension FoodJournalVC : UITableViewDataSource {
+    //determine number of cells
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.foodItems.count
+    }
+    //ask table view cell that it should display in each row
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //Get food from array
+        let food = self.foodItems[indexPath.row]
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.reusableCellIdentifier, for: indexPath) as! FoodCell
+        cell.foodCellContent(at: food)
+        
+        return cell
+    }
+}
+
+
+//MARK:- UITableViewDelegate Methods
+
+//manage user intereaction with tableview
+extension FoodJournalVC: UITableViewDelegate {
+    //take action on selected row
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //Get index of what table is select
+        foodRows = indexPath.row
+        //prefromSegues
+        self.performSegue(withIdentifier: K.goToDetailFoodVCSegues, sender: self)
+    }
     
-    
+
     //swipe to left action
-    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration?
-    {
-        //Create swipe action
-        
-        //UIContextualAction, allow an action to display when user swipe
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        // Create swipe action
+        // UIContextualAction, allow an action to display when user swipe
         //.destructive, action to perform delete data/ destructive task
-        let action = UIContextualAction(style: .destructive, title: "Delete")
-        { (action, view, completionHandler) in
+        let action = UIContextualAction(style: .destructive, title: "Delete") { [weak self] (action, view, completionHandler) in
             
-            //1. Which food to delete
-            let foodToRemove = self.items![indexPath.row]
+            //1. Determine which food to delete
+            guard let foodToRemove = self?.foodItems[indexPath.row] else { fatalError("Cannot remove Food that has been selected ")}
             
-            //2. Remove the move
-            self.context.delete(foodToRemove)
-            
-            //3. Save the data
-            do
-            {
-            try self.context.save()
-            }
-            catch
-            {
-                print(error)
-            }
-            
+            //2. Remove the selected food object and save current context
+            self?.persistentManager.deleteContext(for: foodToRemove)
             //4. Refetch the data
-            self.fetchFood()
+            self?.fetchFood()
         }
-       
-        
-        
         return UISwipeActionsConfiguration(actions: [action])
+    }
+}
+
+//MARK:- Private methods
+
+extension FoodJournalVC {
+    
+    private func renderView() {
+        //listening for any notifcation that already send by sender from another VC
+        NotificationCenter.default.addObserver(self, selector: #selector(fetchFood), name: NSNotification.Name(rawValue: "load"), object: nil)
+        
+        //register custom design n file to be able to use in table view
+        tableView.register(UINib(nibName: "FoodCell", bundle: nil), forCellReuseIdentifier: K.reusableCellIdentifier)
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 600
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+    }
+    
+    @objc private func fetchFood() {
+        foodItems = []
+        //Fetch the data from Core Data to display in the table view
+        do {
+            //assign fetch data to array
+            //fetch request from food model, grab all the data we have
+            self.foodItems = try persistentManager.context.fetch(Food.fetchRequest())
+            //make ui changes in main thread, to prevent perforamance impact
+            DispatchQueue.main.async {
+                //reload table view after get some data
+                self.tableView.reloadData()
+            }
+        } catch {
+            print(error.localizedDescription)
+        }
     }
 }
